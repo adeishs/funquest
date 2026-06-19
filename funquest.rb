a6165329d8a896dfd1c5627a3d392523de5273af
ade ishs <adei@logicly.com.au>
2026-06-19 15:20:31 +1000

    Generate solutions

--- funquest.rb
+++ funquest.rb
@@ -7,26 +7,39 @@ def gen_roots(min, max)
 end
 
 def gen_mult
-  ([1] * 16 + [2] * 8 + [3] * 2 + [4]).sample
+  ([1] * 16 + [2] * 8 + [3]).sample
 end
 
-def gen_solutions(num_of_questions)
+def gen_y0(num_of_questions)
   (1..num_of_questions).map do |i|
     {
-      roots: gen_roots(1 + ((i - 1) * 1.25).to_i, i * 3),
+      roots: gen_roots(1 + ((i - 1) * 1.25).to_i, i * 2).sort,
       mults: Array.new(2) { gen_mult }
     }
   end
 end
 
+def calc_coeff(param)
+  mm = param[:mults].reduce(:*)
+  {
+    a: mm,
+    b: -mm * param[:roots].sum,
+    c: mm * param[:roots].reduce(:*)
+  }
+end
+
+def calc_extremum(coeff)
+  x = 0.5 * -coeff[:b] / coeff[:a]
+  Complex.new(x, coeff[:a] * (x**2) + coeff[:b] * x + coeff[:c])
+end
+
 def gen_questions(num_of_questions)
-  gen_solutions(num_of_questions).map do |p|
-    mm = p[:mults].reduce(:*)
+  gen_y0(num_of_questions).map do |p|
+    coeff = calc_coeff(p)
     {
       **p,
-      a: mm,
-      b: -mm * p[:roots].sum,
-      c: mm * p[:roots].reduce(:*)
+      **coeff,
+      extremum: calc_extremum(coeff)
     }
   end
 end
@@ -39,20 +52,30 @@ def sup_one(num)
   num.abs == 1 ? '' : num.abs.to_s
 end
 
-def fmt_question(question)
+def fmt_function(question)
   format(
-    'f(x) = %<a>sx^2%<b>sx%<c>s',
+    '%<a>sx^2%<b>sx%<c>s',
     a: sign_sym(question[:a], not_neg_sym: '') + sup_one(question[:a]),
     b: sign_sym(question[:b]) + sup_one(question[:b]),
     c: sign_sym(question[:c]) + question[:c].abs.to_s
   )
 end
 
+srand
 questions = gen_questions(10)
-qstr = questions.map { |q| "\\item $#{fmt_question(q)}$" }.join("\n")
+qstr = questions.map { |q| "\\item $f(x) = #{fmt_function(q)}$" }.join("\n")
+sstr = questions.map do |q|
+  format(
+    '\item %<xs>s; Extremum: $(%<ext>s)$',
+    xs: q[:roots].map { |r| "$x = #{r}$" }.join('; '),
+    ext: q[:extremum].rect.map { |c| format('%<c>g', c: c) }.join(', ')
+  )
+end.join("\n")
 puts <<~'OUT'
   \documentclass[12pt, a4paper]{article}
   \usepackage[margin=3cm]{geometry}
+  \usepackage{pgfplots}
+  \pgfplotsset{width=10cm,compat=1.9}
   \usepackage{setspace}
   \onehalfspacing % Sets document line spacing to 1.5
 
@@ -64,11 +87,25 @@ puts <<~'OUT'
 
   \maketitle
 
-  For each of these functions, find the extremum and plot the graph.
+  For each of these functions:
+  \begin{itemize}
+  \item Find the extremum.
+  \item Find the solutions for $f(x) = 0$.
+  \item Plot the graph.
+  \end{itemize}
 
   \begin{enumerate}
 OUT
 puts qstr
+puts <<~'OUT'
+  \end{enumerate}
+
+  \newpage
+
+  \section*{Solutions}
+  \begin{enumerate}
+OUT
+puts sstr
 puts <<~'OUT'
   \end{enumerate}
   \end{document}
